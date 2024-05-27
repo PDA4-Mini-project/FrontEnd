eba48dcedfb0e9bc4c8e6ee0e0ba4aceb8a05ffc
hyuna <kook980316@gmail.com>
2024-05-28 08:55:01 +0900

    fix: signup 500 error 해결
    _id 못 찾는 현상

--- src/lib/apis/users.jsx
+++ src/lib/apis/users.jsx
@@ -3,11 +3,11 @@ import axios from 'axios';
 const BASE_URL = '/api/users';
 const service = axios.create({ withCredentials: true, baseURL: BASE_URL });
 
-export async function Signup(nickname, email, id, password) {
+export async function Signup(name, email, id, password) {
     const res = await service.post('/signup', {
-        name: nickname,
+        name: name,
         email: email,
-        id: id,
+        _id: id,
         password: password,
     });
     return res.data;
--- src/routes/signup/page.jsx
+++ src/routes/signup/page.jsx
@@ -11,10 +11,12 @@ export default function SignupPage() {
         handleSubmit,
         watch,
         formState: { errors },
-        getValues
+        getValues,
     } = useForm();
     const onSubmit = (data) => {
-        Signup(data.nickname, data.email, data.id, data.password1).then(data=>console.log(data)).catch(err=>console.log(err.data))
+        Signup(data.name, data.email, data.id, data.password1)
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err));
     };
 
     return (
@@ -32,11 +34,11 @@ export default function SignupPage() {
                     <div>
                         <p>닉네임 *(최대 8글자)</p>
                         <input
-                            {...register('nickname', { required: '닉네임을 입력해주세요', maxLength: 8 })}
+                            {...register('name', { required: '닉네임을 입력해주세요', maxLength: 8 })}
                             placeholder="닉네임을 입력하세요"
                             className={common}
                         />
-                        <p className="text-red-500">{errors?.nickname?.message}</p>
+                        <p className="text-red-500">{errors?.name?.message}</p>
                     </div>
                     <div>
                         <p>이메일 *</p>
@@ -63,23 +65,26 @@ export default function SignupPage() {
                                 {...register('password1', { required: '비밀번호를 입력해주세요' })}
                                 placeholder="비밀번호"
                                 className={common}
-                                type='password'
+                                type="password"
                             />
                             <p className="text-red-500">{errors?.password1?.message}</p>
                         </div>
                         <div>
                             <p>비밀번호 확인 *</p>
                             <input
-                                {...register('password2', { required: '비밀번호를 한번 더 입력해주세요', validate: {
-                                    check: (val) => {
-                                    if (getValues("password1") !== val) {
-                                        return "비밀번호가 일치하지 않습니다.";
-                                    }
+                                {...register('password2', {
+                                    required: '비밀번호를 한번 더 입력해주세요',
+                                    validate: {
+                                        check: (val) => {
+                                            if (getValues('password1') !== val) {
+                                                return '비밀번호가 일치하지 않습니다.';
+                                            }
+                                        },
                                     },
-                                }, })}
+                                })}
                                 placeholder="비밀번호 확인"
                                 className={common}
-                                type='password'
+                                type="password"
                             />
                             <p className="text-red-500">{errors?.password2?.message}</p>
                         </div>
