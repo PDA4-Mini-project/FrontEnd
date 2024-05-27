ab45e646501a5e4304fec097bc3c702e0f43b693
hyuna333 <kook980316@gmail.com>
2024-05-27 22:48:36 +0900

    feat: react-hook-form을 통해 validation check

--- src/routes/signup/page.jsx
+++ src/routes/signup/page.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import FormButton from '../../components/FormButton';
 import NavBar from '../../components/Navbar';
+import { useForm } from 'react-hook-form';
+import { Signup } from '../../lib/apis/users';
 
 export default function SignupPage() {
     const common = 'rounded-full border-2 border-[#DBDAD3] w-full h-12 px-4';
-    const signupSubmit = () => {};
-    const [nickname, setNickname] = useState('');
-    const [email, setEmail] = useState('');
-    const [id, setId] = useState('');
-    const [password1, setPassword1] = useState('');
-    const [password2, setPassword2] = useState('');
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+        getValues
+    } = useForm();
+    const onSubmit = (data) => {
+        Signup(data.nickname, data.email, data.id, data.password1).then(data=>console.log(data)).catch(err=>console.log(err.data))
+    };
 
     return (
         <>
@@ -19,67 +25,66 @@ export default function SignupPage() {
                     <p className="font-bold text-3xl mb-6">회원가입</p>
                     <p className="text-[#777771] mb-10">쏙쏙정원에 오신 것을 환영합니다!</p>
                 </div>
-                <form className="bg-white py-14 px-12 rounded-3xl flex-col space-y-6 justify-items-center">
+                <form
+                    className="bg-white py-14 px-12 rounded-3xl flex-col space-y-6 justify-items-center"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <div>
-                        <p>닉네임 *</p>
+                        <p>닉네임 *(최대 8글자)</p>
                         <input
-                            onChange={(e) => setNickname(e.target.value)}
-                            type="text"
-                            name="name"
-                            id="name"
+                            {...register('nickname', { required: '닉네임을 입력해주세요', maxLength: 8 })}
                             placeholder="닉네임을 입력하세요"
                             className={common}
                         />
+                        <p className="text-red-500">{errors?.nickname?.message}</p>
                     </div>
                     <div>
                         <p>이메일 *</p>
                         <input
-                            onChange={(e) => setEmail(e.target.value)}
-                            type="text"
-                            name="email"
-                            id="email"
+                            {...register('email', { required: '이메일을 입력해주세요', pattern: /^\S+@\S+$/i })}
                             placeholder="이메일을 입력하세요"
                             className={common}
                         />
+                        <p className="text-red-500">{errors?.email?.message}</p>
                     </div>
                     <div>
                         <p>아이디 *</p>
                         <input
-                            onChange={(e) => setId(e.target.value)}
-                            type="text"
-                            name="id"
-                            id="id"
+                            {...register('id', { required: '아이디를 입력하세요' })}
                             placeholder="아이디를 입력하세요"
                             className={common}
                         />
+                        <p className="text-red-500">{errors?.id?.message}</p>
                     </div>
                     <div className="flex justify-between">
                         <div>
                             <p>비밀번호 *</p>
                             <input
-                                onChange={(e) => setPassword1(e.target.value)}
-                                type="text"
-                                name="password1"
-                                id="password1"
+                                {...register('password1', { required: '비밀번호를 입력해주세요' })}
                                 placeholder="비밀번호"
                                 className={common}
+                                type='password'
                             />
-                            <p className="text-[#5E5F59]">비밀번호를 입력해주세요</p>
+                            <p className="text-red-500">{errors?.password1?.message}</p>
                         </div>
                         <div>
                             <p>비밀번호 확인 *</p>
                             <input
-                                onChange={(e) => setPassword2(e.target.value)}
-                                type="text"
-                                name="password2"
-                                id="password2"
+                                {...register('password2', { required: '비밀번호를 한번 더 입력해주세요', validate: {
+                                    check: (val) => {
+                                    if (getValues("password1") !== val) {
+                                        return "비밀번호가 일치하지 않습니다.";
+                                    }
+                                    },
+                                }, })}
                                 placeholder="비밀번호 확인"
                                 className={common}
+                                type='password'
                             />
-                            <p className="text-[#5E5F59]">비밀번호를 한번 더 입력해주세요</p>
+                            <p className="text-red-500">{errors?.password2?.message}</p>
                         </div>
                     </div>
-                    <FormButton text="회원가입" able={true} func={signupSubmit} />
+                    <FormButton text="회원가입" able={true} />
                 </form>
             </div>
         </>
