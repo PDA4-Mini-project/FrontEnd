30428748a566566e5f46656c5f4a3ad01cf0f586
hyuna <kook980316@gmail.com>
2024-05-27 15:07:22 +0900

    feat: 회원가입 페이지  router 추가

--- src/main-router.jsx
+++ src/main-router.jsx
@@ -1,12 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
-import MainPage from "./routes/page";
+import { createBrowserRouter } from 'react-router-dom';
+import MainPage from './routes/page';
+import SignupPage from './routes/signup/page';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainPage />,
-    index: true
-  }
-])
+    {
+        path: '/',
+        element: <MainPage />,
+        index: true,
+    },
+    {
+        path: '/signup',
+        element: <SignupPage />,
+    },
+]);
 
 export default router;
