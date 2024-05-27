6a0caf7bc2e96f9613df90b45657b46c509778dc
hyuna333 <kook980316@gmail.com>
2024-05-27 22:48:09 +0900

    fix: button을 type submit으로 변경

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -1,15 +1,11 @@
 export default function FormButton(props) {
     const text = props.text;
     const able = props.able;
-    const func = props.func;
     const common = 'w-72 h-14 rounded-lg flex items-center justify-center m-auto';
 
     return (
-        <div
-            className={`${common} ${able ? 'bg-main-green text-white hover:brightness-75 over:cursor-pointer' : 'bg-[#E0E0E0] text-black'}`}
-            onClick={func}
-        >
+        <button type="submit" className={`${common} ${able ? 'bg-main-green text-white hover:brightness-75 over:cursor-pointer' : 'bg-[#E0E0E0] text-black'}`}>
             <p>{text}</p>
-        </div>
+        </button>
     );
 }
