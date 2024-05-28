b729aa951c10e083e00f5582811b9b6d7476bc3b
hyuna <kook980316@gmail.com>
2024-05-28 13:29:48 +0900

    wip: modal 만들기 전에 잠시 불러오기만 한 상태

---
+++ src/components/ReviewModal.jsx
@@ -0,0 +1,10 @@
+export default function ReviewModal() {
+    return (
+        <div>
+            <div>
+                <h1>정원사</h1>
+                <p>정원사님의 재능기부는 어땠나요?</p>
+            </div>
+        </div>
+    );
+}
--- src/routes/garden/page.jsx
+++ src/routes/garden/page.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import NavBar from '../../components/Navbar';
+import ReviewModal from '../../components/ReviewModal';
 
 export default function GardenPage() {
+    const [modalShow, setModalShow] = useState(false);
     return (
         <>
             <NavBar />
             <div>
                 <h1>여기서 정원 리스트 보여줄것입니다</h1>
+                <ReviewModal show={modalShow} onHide={() => setModalShow(false)} />
             </div>
         </>
     );
