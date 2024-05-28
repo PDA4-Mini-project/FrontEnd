c85c5ae43b85abd345cc487babd5f67937e0906d
hyuna <kook980316@gmail.com>
2024-05-28 16:34:47 +0900

    chore: 정원 카드 확인용 코드

--- src/routes/garden/page.jsx
+++ src/routes/garden/page.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import NavBar from '../../components/Navbar';
 import ReviewModal from '../../components/ReviewModal';
+import GardenCard from '../../components/GardenCard';
 
 export default function GardenPage() {
     const [modalShow, setModalShow] = useState(false);
@@ -10,6 +11,7 @@ export default function GardenPage() {
             <button onClick={() => setModalShow(true)}>모달 열려라</button>
             <div>
                 <h1>여기서 정원 리스트 보여줄것입니다</h1>
+                <GardenCard />
                 {modalShow ? <ReviewModal onHide={() => setModalShow(false)} /> : null}
             </div>
         </>
