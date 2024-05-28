e105f5d66ecf7800ec5702117f36ecd70eeba38e
hyuna <kook980316@gmail.com>
2024-05-28 15:05:06 +0900

    chore: 모달 사용 확인용

--- src/routes/garden/page.jsx
+++ src/routes/garden/page.jsx
@@ -7,9 +7,10 @@ export default function GardenPage() {
     return (
         <>
             <NavBar />
+            <button onClick={() => setModalShow(true)}>모달 열려라</button>
             <div>
                 <h1>여기서 정원 리스트 보여줄것입니다</h1>
-                <ReviewModal show={modalShow} onHide={() => setModalShow(false)} />
+                {modalShow ? <ReviewModal onHide={() => setModalShow(false)} /> : null}
             </div>
         </>
     );
