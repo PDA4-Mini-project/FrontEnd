95ca380c50da6c7992471d095a46c22318da61ac
sooyeon-kr <sooyeon7027@naver.com>
2024-05-29 14:30:39 +0900

    feat : page.jsx - 정원 입장 시 내 비디오와 마이크 (로컬) 활성화

--- src/routes/garden/inside/page.jsx
+++ src/routes/garden/inside/page.jsx
@@ -3,19 +3,39 @@ import { MicFill } from 'react-bootstrap-icons';
 import { MicMuteFill } from 'react-bootstrap-icons';
 import { CameraVideoFill } from 'react-bootstrap-icons';
 import { CameraVideoOffFill } from 'react-bootstrap-icons';
-import { useState } from 'react';
-
+import { useEffect, useState, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { io } from 'socket.io-client';
+import adapter from 'webrtc-adapter';
 export default function GardenInsidePage() {
     const [micOn, setMicOn] = useState(true);
     const [cameraOn, setCameraOn] = useState(true);
+    const roomId = useSelector((state) => state.garden.roomId);
+    const localVideoRef = useRef(null);
+    const [localStream, setLocalStream] = useState(null);
 
+    useEffect(() => {
+        navigator.mediaDevices
+            .getUserMedia({ video: true, audio: true })
+            .then((stream) => {
+                setLocalStream(stream);
+                if (localVideoRef.current) {
+                    localVideoRef.current.srcObject = stream;
+                }
+            })
+            .catch((error) => {
+                console.error('Error accessing media devices.', error);
+            });
+    }, []);
     return (
         <div className="flex flex-col h-dvh">
             <NavBar />
             <div className="bg-main-green w-full grid grid-cols-3 p-5 grow gap-5">
                 <div className="bg-white col-span-2">여기 상대 화면</div>
                 <div className="grid gap-5">
-                    <div className="bg-white">여기 나</div>
+                    <div className="bg-white">
+                        <video ref={localVideoRef} autoPlay playsInline className="w-full h-full" />
+                    </div>
                     <div className="bg-white rounded-3xl">여기 채팅 보여줄 자리</div>
                 </div>
             </div>
