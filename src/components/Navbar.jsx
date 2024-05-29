16ff08ccc09d8518d0cf930fdd8c63caf5874794
hyuna <kook980316@gmail.com>
2024-05-29 14:42:18 +0900

    feat: 로그인 됐을 때는 logout 버튼을 보여주도록 변경

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../public/Logo.svg';
 import NavButton from './NavButton';
+import { useEffect, useState } from 'react';
+import { PersonCircle } from 'react-bootstrap-icons';
 
 export default function NavBar() {
+    const [userId, setUserId] = useState('');
+    useEffect(() => {
+        setUserId(sessionStorage.getItem('userId'));
+    }, []);
+    const navigate = useNavigate();
+
     return (
-        <div className="flex justify-between mx-32 my-6">
+        <div className="flex justify-between items-center mx-32 my-6">
             <img src={logo} />
             <div className="flex gap-12">
                 <Link to="/garden">정원</Link>
                 <Link>꽃설명</Link>
             </div>
-            <div className="flex gap-3">
-                <NavButton text="Login" color="white" naviLink="/login" />
-                <NavButton text="Signup" color="green" naviLink="/signup" />
+            <div>
+                {userId ? (
+                    <div className="flex gap-3">
+                        <NavButton text="Logout" color="green" naviLink="/signup" />
+                        <PersonCircle
+                            className="w-10 h-10 fill-current text-main-green hover:cursor-pointer"
+                            onClick={() => navigate('/profile')}
+                        />
+                    </div>
+                ) : (
+                    <div className="flex gap-3">
+                        <NavButton text="Login" color="white" naviLink="/login" />
+                        <NavButton text="Signup" color="green" naviLink="/signup" />
+                    </div>
+                )}
             </div>
         </div>
     );
