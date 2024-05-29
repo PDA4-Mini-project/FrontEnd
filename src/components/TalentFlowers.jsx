f9f838b78f1897cd4ace0f5d10abedea4e8335a4
hyuna333 <kook980316@gmail.com>
2024-05-30 00:00:46 +0900

    refactor: 더미 데이터 꽃들 map으로 보여주기

--- src/components/TalentFlowers.jsx
+++ src/components/TalentFlowers.jsx
@@ -18,25 +18,30 @@ export default function TalentFlowers() {
         },
     ];
     const flowerName = {
-      기술: "튤립",
-      상담: '라벤더',
-      자기개발: '연꽃',
-      예술: '장미',
-      언어: '아이리스',
-      생활: '해바라기'
-    }
-    
+        기술: '튤립',
+        상담: '라벤더',
+        자기개발: '연꽃',
+        예술: '장미',
+        언어: '아이리스',
+        생활: '해바라기',
+    };
+
     return (
         <div className="bg-white mt-12 p-12 grid gap-12">
             <p className="font-bold text-xl text-center">나의 재능꽃</p>
-            <div className="flex justify-center space-x-12">
-                <img className="w-36 h-56" src="https://images.unsplash.com/photo-1560790671-b76ca4de55ef?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8JUVBJUJEJTgzfGVufDB8fDB8fHww" />
-                <div className="w-full flex-col content-end space-y-2">
-                    <p className="font-extrabold text-xl">라벤더(상담)</p>
-                    <p className="font-bold text-lg">Lv.5</p>
-                    <div>게이지가 있을 예정</div>
+            {flowers.map((el, i) => (
+                <div className="flex justify-center space-x-12" key={i}>
+                    <img
+                        className="w-36 h-56"
+                        src="https://images.unsplash.com/photo-1560790671-b76ca4de55ef?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8JUVBJUJEJTgzfGVufDB8fDB8fHww"
+                    />
+                    <div className="w-full flex-col content-end space-y-2">
+                        <p className="font-extrabold text-xl">{flowerName[el.theme_name]}({el.theme_name})</p>
+                        <p className="font-bold text-lg">Lv.{el.level}</p>
+                        <div>게이지가 있을 예정</div>
+                    </div>
                 </div>
-            </div>
+            ))}
         </div>
     );
 }
