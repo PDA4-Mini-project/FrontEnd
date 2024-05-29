3f8f0ad5fb8883c3f2d31a734773bdc9e67f5131
hyuna <kook980316@gmail.com>
2024-05-29 09:12:19 +0900

    feat: 정원 생성 모달 초기 레이아웃

---
+++ src/components/GardenCreateModal.jsx
@@ -0,0 +1,83 @@
+import { useForm } from 'react-hook-form';
+import CategoryTag from './CategoryTag';
+import FuncButton from './FuncButton';
+import { useState } from 'react';
+import FormButton from './FormButton';
+
+export default function GardenCreateModla(props) {
+    const onHide = props.onHide;
+    const handleModalClick = (e) => {
+        if (e.target === e.currentTarget) {
+            onHide();
+        }
+    };
+
+    // 방 생성 관련 state들
+    const [gardenCategory, setGardenCategory] = useState('');
+
+    const selectCategory = (category) => {
+        setGardenCategory(category);
+    };
+    // 방 생성 폼
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <div
+            className="z-10 bg-black/30 w-screen h-screen fixed left-0 top-0 flex justify-center items-center"
+            onClick={handleModalClick}
+        >
+            <div className="w-[648px] h-fit bg-white py-14 px-16 rounded-3xl grid justify-items-center gap-9">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex space-x-6">
+                        <CategoryTag category="기술" selectCategory={selectCategory} />
+                        <CategoryTag category="상담" selectCategory={selectCategory} />
+                        <CategoryTag category="자기계발" selectCategory={selectCategory} />
+                        <CategoryTag category="예술" selectCategory={selectCategory} />
+                        <CategoryTag category="언어" selectCategory={selectCategory} />
+                        <CategoryTag category="생활" selectCategory={selectCategory} />
+                    </div>
+                    <div>
+                        <p>정원 이름 *</p>
+                        <input
+                            {...register('title', { required: '정원 이름을 입력해주세요' })}
+                            placeholder="정원 이름을 입력하세요"
+                            className="rounded-full border-2 border-[#DBDAD3] w-full h-12 px-4"
+                        />
+                        <p className="text-red-500">{errors?.title?.message}</p>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        <div>
+                            <input type="radio" value="30" {...register('time')} />
+                            30분
+                        </div>
+                        <div>
+                            <input type="radio" value="60" {...register('time')} />
+                            60분
+                        </div>
+                        <div>
+                            <input type="radio" value="90" {...register('time')} />
+                            90분
+                        </div>
+                        <div>
+                            <input type="radio" value="120" {...register('time')} />
+                            120분
+                        </div>
+                    </div>
+
+                    <div className="flex pr-6">
+                        <FormButton text="정원 만들기" able={true} />
+                        <FuncButton size="sm" text="닫기" func={onHide} />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
