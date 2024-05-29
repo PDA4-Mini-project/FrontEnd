6455ecc60eda00562fe03a3743efc4a4433b3617
hyuna <kook980316@gmail.com>
2024-05-29 09:41:49 +0900

    feat: 정원 시간도 선택할 수 있는 기능 추가

--- src/components/GardenCreateModal.jsx
+++ src/components/GardenCreateModal.jsx
@@ -24,10 +24,14 @@ export default function GardenCreateModla(props) {
         register,
         handleSubmit,
         formState: { errors },
+        watch,
     } = useForm();
+    const selectedTime = watch('time');
 
     const onSubmit = (data) => {
         console.log(data);
+        console.log(gardenCategory);
+        onHide();
     };
 
     const categories = ['기술', '상담', '자기계발', '예술', '언어', '생활'];
@@ -37,8 +41,8 @@ export default function GardenCreateModla(props) {
             className="z-10 bg-black/30 w-screen h-screen fixed left-0 top-0 flex justify-center items-center"
             onClick={handleModalClick}
         >
-            <div className="w-[648px] h-fit bg-white py-14 px-16 rounded-3xl grid justify-items-center gap-9">
-                <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="w-[648px] h-fit bg-white py-14 px-16 rounded-3xl grid justify-items-center">
+                <form onSubmit={handleSubmit(onSubmit)} className="grid gap-y-6">
                     <div className="flex space-x-6">
                         {categories.map((el, i) => (
                             <CategoryTagButton
@@ -58,23 +62,19 @@ export default function GardenCreateModla(props) {
                         />
                         <p className="text-red-500">{errors?.title?.message}</p>
                     </div>
-                    <div className="grid grid-cols-2">
-                        <div>
-                            <input type="radio" value="30" {...register('time')} />
-                            30분
-                        </div>
-                        <div>
-                            <input type="radio" value="60" {...register('time')} />
-                            60분
-                        </div>
-                        <div>
-                            <input type="radio" value="90" {...register('time')} />
-                            90분
-                        </div>
-                        <div>
-                            <input type="radio" value="120" {...register('time')} />
-                            120분
-                        </div>
+                    <div className="grid grid-cols-2 justify-items-center gap-y-3">
+                        {['30', '60', '90', '120'].map((value) => (
+                            <label key={value} className="flex items-center cursor-pointer">
+                                <input type="radio" value={value} {...register('time')} className="hidden" />
+                                <div
+                                    className={`w-24 h-10 flex items-center justify-center rounded-3xl ${
+                                        selectedTime === value ? 'bg-main-green text-white' : 'bg-gray-200'
+                                    }`}
+                                >
+                                    {value}분
+                                </div>
+                            </label>
+                        ))}
                     </div>
 
                     <div className="flex pr-6">
